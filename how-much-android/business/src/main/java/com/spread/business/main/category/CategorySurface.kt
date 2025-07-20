952ac311d2183369311d746bd59d216edacb2438
linforx <linforx@163.com>
2025-07-21 00:53:12 +0800

    fix code style

--- how-much-android/business/src/main/java/com/spread/business/main/category/CategorySurface.kt
+++ how-much-android/business/src/main/java/com/spread/business/main/category/CategorySurface.kt
@@ -14,8 +14,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
